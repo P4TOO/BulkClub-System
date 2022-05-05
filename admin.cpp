e9bb4dab91a35959d8aabd3fd81aff01635ba9fa
Gaby P <gabrielle.a.palar@gmail.com>
2022-05-04 20:38:18 -0700

    edited salesreport table

--- admin.cpp
+++ admin.cpp
@@ -18,7 +18,7 @@ Admin::~Admin()
 void Admin::on_pushButton_clicked()
 {
     QSqlDatabase db=QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("/Users/adriangomez/desktop/Bulk Project/BulkClub-System/BulkClubProject.db");
+    db.setDatabaseName("C:/Users/gpala_zdi8b1w/BulkClub-System/BulkClubProject.db");
 
     if(!db.open())
     {

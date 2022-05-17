bad1f9c365505030ca939a58ba53db2a453f7871
zzfp <zacallyn@gmail.com>
2022-05-16 23:25:14 -0700

    tax added to sales report total

--- salesreport.h
+++ salesreport.h
@@ -18,7 +18,7 @@ public:
 private slots:
     void on_MemberTypePushButton_clicked();
 
-    void on_pushButton_clicked();
+    void on_DatePushButton_clicked();
 
 private:
     Ui::SalesReport *ui;

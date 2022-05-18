46eff37576f26c27e581e05e9ca24fa85db1c971
zzfp <zacallyn@gmail.com>
2022-05-18 12:09:23 -0700

    added customer name and member count by type to salesreport date search

--- salesreport.cpp
+++ salesreport.cpp
@@ -150,7 +150,7 @@ void SalesReport::on_DatePushButton_clicked()
     QSqlQueryModel * model = new QSqlQueryModel();
    //model is readonly access to query results
     QSqlQuery query(db);
-    query.prepare("SELECT * FROM Sales_Record WHERE Purchase_Date =(:date)");
+    query.prepare("SELECT Sales_Record.Purchase_Date,Members.Member_Name,Members.Membership_Type,Sales_Record.Membership_Number,Sales_Record.Item_Purchased,Sales_Record.Sales_Price,Sales_Record.Quantity_Purchased FROM Sales_Record LEFT JOIN Members ON Sales_Record.Membership_Number = Members.Membership_ID WHERE Purchase_Date=(:date)");
     query.bindValue(":date", date);
 
     query.exec(); //query must be active before being moved into the model
@@ -158,18 +158,29 @@ void SalesReport::on_DatePushButton_clicked()
 
     QSqlRecord totalSalesRecord;//this record will hold an individual user data row
     int totalSalesIterator = 1;
+    int execAmount=0;
+    int regAmount=0;
     double salesPrice;
     double salesQuantity;
     double runningTotal = 0;
     do{
         totalSalesRecord = model->record(totalSalesIterator); //sets record to the row of the iterator in the model
-        salesPrice = totalSalesRecord.value(3).toDouble(); //value at index 3 in the row should be Sales_Price
-        salesQuantity = totalSalesRecord.value(4).toDouble(); //value at index 4 in the row should be Quantity_Purchased
+        salesPrice = totalSalesRecord.value(5).toDouble(); //value at index 3 in the row should be Sales_Price
+        salesQuantity = totalSalesRecord.value(6).toDouble(); //value at index 4 in the row should be Quantity_Purchased
         runningTotal += salesPrice * salesQuantity;
         totalSalesIterator++;
-    }while (!totalSalesRecord.isNull(3));
+        if(totalSalesRecord.value(2).toString()=="Executive"){
+            execAmount++;
+        }
+        else{
+            regAmount++;
+        }
+    }while (!totalSalesRecord.isNull(4));
     runningTotal += runningTotal * 0.0775;
     QString finalTotal = finalTotal.number(runningTotal,'f',2);//sets a formatted total to a string that can be passed to the totalSalesNum label
+
+    ui->execAmtLabel->setText(QString::number(execAmount));
+    ui->regAmtLabel->setText(QString::number(regAmount));
 
     ui->tableView->setModel(model);
 
